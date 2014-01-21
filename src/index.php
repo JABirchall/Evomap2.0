254d9eb057ccc8edb4f86b9afda2a2987b28d146
DrWhat <DrWhat@cryto.net>
2014-01-21 14:13:38 +0000

    Remove redundant code

--- src/index.php
+++ src/index.php
@@ -2,7 +2,6 @@
 // How i like to load shit.
 require_once('classes/init.class.php');
 $i = NEW init(1); // Use init(1) to init HTML elements.
-$DB = NEW database($DB_ip, $DB_database, $DB_user, $DB_password);
 
 
 echo "<h1> Hello</h1>";
